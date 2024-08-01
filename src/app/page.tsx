01e5a227bdb051619c674246abcf7ff75c523058
Danilo Melo <danilomarquesdemelo@gmail.com>
2024-08-01 13:26:29 -0300

    chore: intall nextjs

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+export default function page() {
+  return (
+    <main>
+      <h1>Home page teste</h1>
+
+      <div>
+        <p>
+          paragrafo de teste
+        </p>
+      </div>
+    </main>
+  )
+}
